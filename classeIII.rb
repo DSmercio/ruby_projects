37758b9bfbe1fcee994815db015cf21e4b2f3b6a
QAmercio <eduardo.mercio@gmail.com>
2018-08-28 18:26:12 -0300

    atualizacoes, tipo: commit

--- classeIII.rb
+++ classeIII.rb
@@ -1,3 +1,4 @@
+=begin
 module ModuloNome
     #serve para agrupar classes
     #serve para agrupar constantes
@@ -19,3 +20,30 @@ end
 objeto = ClassName.new
 
 objeto.metodo_padrao
+=end
+
+class Cachorro
+    def latir
+        puts'au au au'
+    end
+end
+
+class CachorroGrande
+    def latir
+        puts'au au'
+    end
+end
+
+class Pessoa
+    def agarra_cachorro(cachorro)
+        cachorro.latir
+    end
+end
+
+c1=Cachorro.new
+c2=CachorroGrande.new
+
+p=Pessoa.new
+
+p.agarra_cachorro(c1)
+p.agarra_cachorro(c2)
---
+++ testes/features/specs/cenario.feature
@@ -0,0 +1,13 @@
+# language : pt
+
+Funcionalidade: Calculo da subtracao e soma
+
+Cenario: Fazer subtracao
+Dado que que tenho 10 laranjas
+Quando eu como 2 laranjas
+Entao eu vejo quantas laranjas sobraram
+
+Cenario: Fazer soma
+Dado que que tenho 10 laranjas
+Quando eu compro 5 laranjas
+Entao eu vejo quantas laranjas eu tenho
